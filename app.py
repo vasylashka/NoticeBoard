487b3312b6c6b33d44429f7241ef78b205f87d95
vasylashka <vasylashka@gmail.com>
2023-11-23 13:57:17 +0200

    Refresh gitignore, add database with docker, configure swagger

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_mongoengine import MongoEngine
+from flask_swagger_ui import get_swaggerui_blueprint
+from ORM.user import User
 
 app = Flask(__name__)
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'NoticeBoard',
+    'host': 'mongo'
+}
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "NoticeBoard API"
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+
+db = MongoEngine(app)
 
 
 @app.route('/api/v1/hello-world-3')
